6e711821c3a4cb8878bba0e59bfe0e7a785a6889
Ali_Sdg90 <115698554+Ali-Sdg90@users.noreply.github.com>
2023-01-14 00:14:15 +0330

    add and fix apply btn / add changeColor / add multi start point with different colors / else

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const gameBox = document.getElementById("game-box");
 let endLoop;
-let gameSize = 5;
+let gameSize = 10;
 let delay = 100;
-let maxLoopCounter = 10;
+let maxLoopCounter = 20;
+let oneWayMethode = true;
+let changeColor = true;
+let showDebug = true;
+let showNums = false;
 let Blockes;
+const backgroundColor = "rgb(49, 49, 49)";
+document
+    .querySelector(":root")
+    .style.setProperty("--game-background", backgroundColor);
+const hexChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+function colorChanger() {
+    let hexSaver = "#";
+    for (let i = 0; i < 6; i++) {
+        hexSaver += hexChar[Math.floor(Math.random() * 16)];
+    }
+    spreadColorRGB = `rgb(${hexToRgb(hexSaver).join(", ")})`;
+    return hexSaver;
+}
 
 const hexToRgb = (hex) =>
     hex
@@ -16,28 +33,40 @@ const hexToRgb = (hex) =>
         .map((x) => parseInt(x, 16));
 
 let spreadColorHEX = "#ffc0cb";
-let spreadColorRGB = `rgb(${hexToRgb(spreadColorHEX).join(", ")})`;
+spreadColorHEX = colorChanger();
 document.getElementById("game-size").value = gameSize;
 document.getElementById("spread-speed").value = delay;
 document.getElementById("spread-color").value = spreadColorHEX;
 document.getElementById("game-accuracy").value = maxLoopCounter;
+if (changeColor) document.getElementById("change-color").checked = true;
+else document.getElementById("change-color").checked = false;
+if (showDebug) document.getElementById("show-debug").checked = true;
+else document.getElementById("show-debug").checked = false;
+if (showNums) document.getElementById("show-nums").checked = true;
+else document.getElementById("show-nums").checked = false;
+if (oneWayMethode) document.getElementById("spread-1-way").checked = true;
+else document.getElementById("spread-1-way").checked = false;
 
 function makeGround() {
     for (let i = 0; i < gameSize * gameSize; i++) {
         const addBlock = document.createElement("div");
-        // addBlock.textContent = i;
+        if (showNums) addBlock.textContent = i;
         gameBox.appendChild(addBlock);
     }
     gameBox.style.gridTemplateRows = `repeat(${gameSize},1fr)`;
     gameBox.style.gridTemplateColumns = `repeat(${gameSize},1fr)`;
     Blockes = Array.from(document.querySelectorAll("#game-box div"));
+    Blockes.forEach(function (block) {
+        block.style.background = backgroundColor;
+    });
     for (let i in Blockes) {
         Blockes[i].addEventListener("click", function () {
-            if (Blockes[i].style.background != spreadColorRGB) {
+            if (Blockes[i].style.background == backgroundColor) {
+                if (changeColor) spreadColorHEX = colorChanger();
                 Blockes[i].style.background = spreadColorRGB;
                 Blockes[i].style.border = `0.25vh solid ${oppositeColor()}`;
                 endLoop = false;
-                wayFinder(i);
+                wayFinder(i, spreadColorRGB);
             }
         });
     }
@@ -53,7 +82,7 @@ function oppositeColor() {
 }
 
 let randomDirection;
-function wayFinder(startPoint) {
+function wayFinder(startPoint, blockColor) {
     startPoint = Number(startPoint);
     randomDirection = Math.floor(Math.random() * 4);
     loopChecker(startPoint);
@@ -63,64 +92,64 @@ function wayFinder(startPoint) {
             if (
                 Blockes[startPoint - 1] &&
                 startPoint % gameSize != 0 &&
-                Blockes[startPoint - 1].style.background != spreadColorRGB
+                Blockes[startPoint - 1].style.background == backgroundColor
             ) {
                 console.log(startPoint, "left");
                 setTimeout(() => {
-                    wayFinder(startPoint - 1);
-                    Blockes[startPoint - 1].style.background = spreadColorRGB;
+                    wayFinder(startPoint - 1, blockColor);
+                    Blockes[startPoint - 1].style.background = blockColor;
                 }, delay);
             } else {
-                wayFinder(startPoint);
+                wayFinder(startPoint, blockColor);
             }
-            break;
+            if (oneWayMethode) break;
         case 1: // right
             if (
                 Blockes[startPoint + 1] &&
                 (startPoint + 1) % gameSize != 0 &&
-                Blockes[startPoint + 1].style.background != spreadColorRGB
+                Blockes[startPoint + 1].style.background == backgroundColor
             ) {
                 console.log(startPoint, "right");
                 setTimeout(() => {
-                    wayFinder(startPoint + 1);
-                    Blockes[startPoint + 1].style.background = spreadColorRGB;
+                    wayFinder(startPoint + 1, blockColor);
+                    Blockes[startPoint + 1].style.background = blockColor;
                 }, delay);
             } else {
-                wayFinder(startPoint);
+                wayFinder(startPoint, blockColor);
             }
-            break;
+            if (oneWayMethode) break;
         case 2: // down
             if (
                 Blockes[startPoint + gameSize] &&
-                Blockes[startPoint + gameSize].style.background !=
-                    spreadColorRGB
+                Blockes[startPoint + gameSize].style.background ==
+                    backgroundColor
             ) {
                 console.log(startPoint, "down");
                 setTimeout(() => {
-                    wayFinder(startPoint + gameSize);
+                    wayFinder(startPoint + gameSize, blockColor);
                     Blockes[startPoint + gameSize].style.background =
-                        spreadColorRGB;
+                        blockColor;
                 }, delay);
             } else {
-                wayFinder(startPoint);
+                wayFinder(startPoint, blockColor);
             }
-            break;
+            if (oneWayMethode) break;
         case 3: // up
             if (
                 Blockes[startPoint - gameSize] &&
-                Blockes[startPoint - gameSize].style.background !=
-                    spreadColorRGB
+                Blockes[startPoint - gameSize].style.background ==
+                    backgroundColor
             ) {
                 console.log(startPoint, "up");
                 setTimeout(() => {
-                    wayFinder(startPoint - gameSize);
+                    wayFinder(startPoint - gameSize, blockColor);
                     Blockes[startPoint - gameSize].style.background =
-                        spreadColorRGB;
+                        blockColor;
                 }, delay);
             } else {
-                wayFinder(startPoint);
+                wayFinder(startPoint, blockColor);
             }
-            break;
+            if (oneWayMethode) break;
     }
 }
 let last20Moves = [];
@@ -142,31 +171,50 @@ function loopChecker(randomDirection) {
     } else {
         last20Moves.shift();
         last20Moves.push(randomDirection);
-        // console.log(last20Moves.join("."));
         endLoop = false;
     }
 }
 // Blockes[Math.floor(Math.random() * Math.pow(gameSize, 2))].click();
 document.getElementById("apply").addEventListener("click", function () {
-    console.log(Blockes.length);
-    console.log(typeof gameSize, gameSize);
-    console.log(typeof delay, delay);
-    console.log(typeof spreadColorRGB, spreadColorRGB);
-    console.log(typeof spreadColorHEX, spreadColorHEX);
-    console.log(typeof maxLoopCounter, maxLoopCounter);
-    gameSize = Number(document.getElementById("game-size").value);
+    if (gameSize != Number(document.getElementById("game-size").value)) {
+        gameBox.textContent = "";
+        gameSize = Number(document.getElementById("game-size").value);
+        makeGround();
+    }
     delay = Number(document.getElementById("spread-speed").value);
     spreadColorHEX = document.getElementById("spread-color").value;
-    spreadColorRGB = `rgb(${hexToRgb(spreadColorHEX).join(", ")})`;
     maxLoopCounter = Number(document.getElementById("game-accuracy").value);
-    gameBox.textContent = "";
-    makeGround();
+    if (document.getElementById("change-color").checked) {
+        changeColor = true;
+    } else {
+        changeColor = false;
+        spreadColorHEX = colorChanger();
+        document.getElementById("spread-color").value = spreadColorHEX;
+    }
+    if (document.getElementById("show-debug").checked) {
+        showDebug = true;
+    } else {
+        showDebug = false;
+    }
+    console.log(document.getElementById("show-nums").checked, showNums);
+    if (document.getElementById("show-nums").checked != showNums) {
+        gameBox.textContent = "";
+        if (showNums) showNums = false;
+        else showNums = true;
+        makeGround();
+    }
+    if (document.getElementById("spread-1-way").checked) {
+        oneWayMethode = true;
+        document.getElementById("game-accuracy").style.PointerEvent = "auto";
+        document.getElementById("game-accuracy").value = 20;
+        document.getElementById("accuracy-form").style.pointerEvents = "auto";
+        maxLoopCounter = 20;
+    } else {
+        oneWayMethode = false;
+        document.getElementById("game-accuracy").value = 3;
+        document.getElementById("accuracy-form").style.pointerEvents = "none";
+        maxLoopCounter = 3;
+    }
     createMaxLoopCounter();
     console.log("=========");
-    console.log(Blockes.length);
-    console.log(typeof gameSize, gameSize);
-    console.log(typeof delay, delay);
-    console.log(typeof spreadColorRGB, spreadColorRGB);
-    console.log(typeof spreadColorHEX, spreadColorHEX);
-    console.log(typeof maxLoopCounter, maxLoopCounter);
 });
